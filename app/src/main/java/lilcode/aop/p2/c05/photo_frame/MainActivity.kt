587fe1129bc0efafa21f5de8985f28812fda861d
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-17 20:41:59 +0900

    Feat: button 2개 추가 및 코드연결

--- app/src/main/java/lilcode/aop/p2/c05/photo_frame/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p2/c05/photo_frame/MainActivity.kt
@@ -2,8 +2,18 @@ package lilcode.aop.p2.c05.photo_frame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val addPhotoButton: Button by lazy{
+        findViewById<Button>(R.id.addPhotoButton)
+    }
+
+    private val startPhotoFrameModeButton: Button by lazy{
+        findViewById(R.id.startPhotoFrameModeButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
