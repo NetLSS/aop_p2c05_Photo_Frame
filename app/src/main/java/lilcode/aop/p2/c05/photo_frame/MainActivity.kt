6d89fc1809c8aaea536459c977b7f16528fbc70a
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-17 20:31:37 +0900

    Initial commit

---
+++ app/src/main/java/lilcode/aop/p2/c05/photo_frame/MainActivity.kt
@@ -0,0 +1,11 @@
+package lilcode.aop.p2.c05.photo_frame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
