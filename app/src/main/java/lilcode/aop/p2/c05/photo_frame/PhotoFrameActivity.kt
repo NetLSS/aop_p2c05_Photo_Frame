f68c4dfb1fb45879ea7f963eedb261fb2ad94c8e
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-18 10:40:50 +0900

    Feat: 전자액자 activity 추가 (매니페스트, 코드, xml)

---
+++ app/src/main/java/lilcode/aop/p2/c05/photo_frame/PhotoFrameActivity.kt
@@ -0,0 +1,12 @@
+package lilcode.aop.p2.c05.photo_frame
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PhotoFrameActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photoframe)
+
+    }
+}
